1df80c4ae68c665f5ca09aa2ba8b0db4ead783ef
crisog <ortega.cpp@gmail.com>
2025-02-25 17:22:47 -0400

    complete until module 11

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -1,17 +1,24 @@
 fn trim_me(input: &str) -> &str {
-    // TODO: Remove whitespace from both ends of a string.
+    input.trim()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this.
+
+    let mut new_string: String = input.to_string();
+    new_string.push_str(" world!");
+
+    new_string
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons".
+    input.replace("cars", "balloons")
 }
 
 fn main() {
     // You can optionally experiment here.
+    println!("{:?}", trim_me("  What's up!"))
 }
 
 #[cfg(test)]
